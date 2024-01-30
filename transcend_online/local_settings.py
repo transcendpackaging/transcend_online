79b83f38a88662fd246b137d5c94fa48481e5300
transcendpackaging <rakib.ahmed@transcendpackaging.co.uk>
2024-01-30 15:14:04 +0000

    initial commit

--- transcend_online/local_settings.py
+++ transcend_online/local_settings.py
@@ -6,14 +6,21 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#	'default': {
+#		'ENGINE': 'django.db.backends.postgresql',
+#		'NAME': 'transcend_online',
+#		'USER': 'postgres',
+#		'PASSWORD': 'Blueprint06!',
+#		'HOST': 'transcend-online.cipyjxysyrgm.eu-west-2.rds.amazonaws.com',
+#	}
+#}
+
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': 'transcend_online',
-		'USER': 'postgres',
-		'PASSWORD': 'Blueprint06!',
-		'HOST': 'transcend-online.cipyjxysyrgm.eu-west-2.rds.amazonaws.com',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
