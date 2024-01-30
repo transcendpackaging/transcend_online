286af017059db90f3a89e09ee44e830d4fc2d24c
transcendpackaging <rakib.ahmed@transcendpackaging.co.uk>
2024-01-30 15:48:58 +0000

    initial commit

--- transcend_online/local_settings.py
+++ transcend_online/local_settings.py
@@ -6,23 +6,23 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': 'transcend_online',
-		'USER': 'postgres',
-		'PASSWORD': 'Blueprint06!',
-		'HOST': 'transcend-online.cipyjxysyrgm.eu-west-2.rds.amazonaws.com',
-		'PORT': '5432',
-	}
-}
-
 #DATABASES = {
-  #  'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': BASE_DIR / 'db.sqlite3',
-  #  }
+#	'default': {
+#		'ENGINE': 'django.db.backends.postgresql',
+#		'NAME': 'transcend_online',
+#		'USER': 'postgres',
+#		'PASSWORD': 'Blueprint06!',
+#		'HOST': 'transcend-online.cipyjxysyrgm.eu-west-2.rds.amazonaws.com',
+#		'PORT': '5432',
+#	}
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
